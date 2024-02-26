5efb5052f0afa52ab8aaf6f11f8aa064886dd73c
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2024-02-26 15:55:53 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,12 +1,49 @@
             System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-        }
-        for (int item : data) {
-}
-        }
-public class RandomDataGenerator {
-
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
         for (int i = 0; i < 10; i++) {
     }
+    public static void main(String[] args) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+
+        for (int item : data) {
+        }
+        int[] data = new int[10];
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    public static void main(String[] args) {
+        for (int item : data) {
+        int[] data = new int[10];
+        }
+        }
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+    }
+            System.out.println("Random Number: " + item);
+    }
+        for (int i = 0; i < 10; i++) {
+
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+        int[] data = new int[10];
+        for (int item : data) {
+
+        for (int item : data) {
+        }
+
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        }
+}
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
